7a5a9f4736e3e9001ed4126f545e8d2561102bad
trsobayf <ferhat.sobay@eu.sony.com>
2012-02-24 01:05:11 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borcly::Application.config.secret_token = '7bddc4b58b23fd9289cf686d3dbdb44cf5fa0336358e98a52207e16e78c27c17bea806e02cc762880d012623a7e27f2120fcffb5160c985ad8e8ffa23ecc071a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Borcly::Application.config.session_store :cookie_store, key: '_borcly_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Borcly::Application.config.session_store :active_record_store
