f0ee122d40d8673d1c894010bb18bf1a737c3f1b
ferhatsb <ferhat.sobay@eu.sony.com>
2012-02-25 19:59:59 +0200

    social

---
+++ db/migrate/20120225160828_create_channels.rb
@@ -0,0 +1,13 @@
+class CreateChannels < ActiveRecord::Migration
+  def change
+    create_table :channels do |t|
+      t.integer :user_id
+      t.string :provider
+      t.string :uid
+      t.string :oauth_token
+      t.string :oauth_token_secret
+
+      t.timestamps
+    end
+  end
+end
