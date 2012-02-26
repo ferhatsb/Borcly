a08e533f8dfb7bb61c2b518c360acddfa2311908
ferhatsb <ferhat.sobay@eu.sony.com>
2012-02-26 17:46:10 +0200

    mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,21 @@ Borcly::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "herokuapp.com",
+      :user_name            => "borcly@gmail.com",
+      :password             => "railsarena",
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = {
+      :host => "herokuapp.com"
+  }
+
 end
